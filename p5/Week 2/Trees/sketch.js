2e201d2843e7c763822aa64e8412a9692b098943
Sean <seanw1996@hotmail.com>
2020-01-23 10:29:56 -0600

    trees

---
+++ p5/Week 2/Trees/sketch.js
@@ -0,0 +1,92 @@
+
+  function setup() {
+
+  createCanvas(720, 400);
+  background('#B8F5FF');
+  noStroke();
+
+  fill('#663D00');
+  rect(100, 350, 30, 400);
+
+  fill('#2B8000')
+  triangle(30, 350, 115, 250, 200, 350);
+
+  fill('#2B8000')
+  triangle(45, 300, 112, 225, 180, 300);
+
+  fill('#2B8000')
+  triangle(60, 250, 112, 200, 160, 250);
+
+
+  fill('#663D00');
+  rect(500, 350, 30, 400);
+
+  fill('#2B8000')
+  triangle(425, 350, 510, 250, 600, 350);
+
+  fill('#2B8000')
+  triangle(435, 300, 510, 225, 575, 300);
+
+  fill('#2B8000')
+  triangle(445, 250, 510, 200, 555, 250);
+
+
+  fill('#663D00');
+  rect(300, 375, 15, 400);
+
+  fill('#2B8000')
+  triangle(250, 375, 307, 335, 355, 375);
+
+  fill('#2B8000')
+  triangle(270, 350, 307, 320, 340, 350);
+
+  fill('#2B8000')
+  triangle(290, 325, 307, 305, 325, 325);
+
+
+
+
+  fill('#FFFE2B');
+  ellipse(650, 75, 100, 100);
+
+  fill(250)
+  ellipse(200, 10, 100, 100)
+
+  fill(250)
+  ellipse(250, 15, 75, 75)
+
+  fill(250)
+  ellipse(120, 15, 75, 75)
+
+  fill(250)
+  ellipse(160, 30, 75, 75)
+
+  fill(250)
+  ellipse(500, 20, 100, 100)
+
+  fill(250)
+  ellipse(550, 25, 75, 75)
+
+  fill(250)
+  ellipse(520, 25, 75, 75)
+
+  fill(250)
+  ellipse(460, 25, 75, 75)
+
+  fill(250)
+  ellipse(390, 100, 80, 80)
+
+  fill(250)
+  ellipse(390, 75, 75, 75)
+
+  fill(250)
+  ellipse(350, 75, 75, 75)
+
+  fill(250)
+  ellipse(325, 100, 75, 75)
+}
+
+
+function draw() {
+  // put drawing code here
+}
