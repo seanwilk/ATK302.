f2af28363221fda2a3ed0bf712cff8f7de0c2835
Sean <seanw1996@hotmail.com>
2020-02-11 11:23:26 -0600

    jkladshfads

--- p5/Week5/anim/sketch.js
+++ p5/Week5/anim/sketch.js
@@ -8,7 +8,9 @@ function setup() {
 function draw() {
   // put drawing code here
   background ('grey');
-  rect (x, 200, 100, 100,);
+  //rect (x, 200, 100, 100,);
+  textSize (40) ;
+  text ("Parasite", x, 100) ;
   x = x + 5 ;
   if (x > width) {
     x = 0 ;
---
+++ p5/Week5/stateMouse/sketch.js
@@ -0,0 +1,43 @@
+var myState = 0 ;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800) ;
+}
+
+function draw() {
+  // put drawing code here
+
+switch(myState){
+  case 0:
+  background("red") ;
+  text("hello world", 100, 100) ;
+  break ;
+
+  case 1:
+  background("green") ;
+  text("hola world", 100, 100);
+  break;
+
+  case 2:
+  background("grey") ;
+  text("bonjour world", 100, 100);
+  break;
+
+  case 3:
+  break ;
+
+  case 4:
+  break ;
+
+}
+
+}
+
+function mouseReleased() {
+  myState = myState + 1 ;
+  if (myState > 4) {
+    myState = 0 ;
+  }
+
+}
