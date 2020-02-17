459717bbf185e711d48aa53d3ea83cfb4d397354
Sean <seanw1996@hotmail.com>
2020-02-17 14:36:20 -0600

    ope

---
+++ p5/Week5/debug/sketch.js
@@ -0,0 +1,40 @@
+var myState = 0 ;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+}
+
+function draw() {
+
+  switch(myState){
+    case 0:
+      background(255, 0, 0);
+      fill('white');
+      textSize(100);
+      text("case 0", width/2, 100);
+      break;
+
+    case 1:
+      background(0, 255, 0);
+      fill('black');
+      textSize(100);
+      text("case 1", 100, 100);
+      break;
+
+    case 2:
+      background(0, 0, 255);
+      fill('gold');
+      textSize(100);
+      text("case 2", 100, 100);
+      break;
+    }
+}
+
+function mouseReleased() {
+  myState = myState + 1 ;
+  if (myState > 2) {
+    myState = 0 ;
+  }
+
+}
